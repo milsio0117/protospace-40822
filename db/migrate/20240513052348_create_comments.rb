9e44237fb4b16e623f0f3cdc525a0817f3f2ae53
milsio0117 <milsio117@gmail.com>
2024-05-14 14:32:24 +0900

    initial commit

--- db/migrate/20240513052348_create_comments.rb
+++ db/migrate/20240513052348_create_comments.rb
@@ -1,10 +1,12 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
-      t.text :content
-      t.references :user
-      t.references :prototype
-      t.timestamps
+    def change
+      create_table :comments do |t|
+        t.text :content
+        t.references :user
+        t.references :prototype
+        t.timestamps
+      end
     end
   end
 end
